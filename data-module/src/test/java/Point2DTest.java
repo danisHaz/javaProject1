92e28e6b302e9f0676f10e6e6124f30069f7631f
danishaz <danishaz112345@gmail.com>
2022-11-07 00:43:25 +0300

    add tests to Point2D

---
+++ data-module/src/test/java/Point2DTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import app.Point2D;
+
+public class Point2DTest {
+    private Point2D point;
+
+    public Point2DTest() {
+        try {
+            point = new Point2D(new double[] {1, 3});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void checkRot() {
+        String testName = "point2D length";
+        assertTrue(testName, String.format("%.4f", point.abs()).equals("3.1623"));
+
+        try {
+            point = Point2D.rot(point, Math.PI);
+            assertTrue(testName, String.format("%.4f", Point2D.add(point, point).abs()).equals("6.3246"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
