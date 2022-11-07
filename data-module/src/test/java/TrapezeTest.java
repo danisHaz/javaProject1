8af9657ab27450c03c62acda6ac37297c6cc2e1a
danishaz <danishaz112345@gmail.com>
2022-11-07 16:14:03 +0300

    add tests to trapeze and change trapeze

---
+++ data-module/src/test/java/TrapezeTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import app.Point2D;
+import app.Trapeze;
+
+public class TrapezeTest {
+    
+    private Point2D[] points;
+    private Trapeze trapeze;
+
+    public TrapezeTest() {
+        try {
+            points = new Point2D[] {
+                new Point2D(),
+                new Point2D(4, 6),
+                new Point2D(11, 6),
+                new Point2D(12, 0)
+            };
+            trapeze = new Trapeze(points);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void checkLength() {
+        String testName = "trapeze length";
+        try {
+            assertTrue(testName, String.format("%.4f", trapeze.length()).equals("32.2939"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(testName, false);
+        }
+    } 
+}
