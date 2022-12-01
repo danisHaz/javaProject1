8243a55500c4f0c34639880db7a94acf7f400b65
danisHaz <43248094+danisHaz@users.noreply.github.com>
2022-12-02 01:11:13 +0300

    [WIP] add templates and main controller

--- web-application-module/src/main/java/spring/io/main/page/MainController.java
+++ web-application-module/src/main/java/spring/io/main/page/MainController.java
@@ -1,13 +1,40 @@
 package spring.io.main.page;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
     
-    @RequestMapping("main")
-    public void start() {
-        
+    @GetMapping("/mainPage")
+    public String getMainPage(Model model) {
+        return "MainPage";
+    }
+
+    @RequestMapping("/addFigures")
+    public String getAddFiguresPage(Model model) {
+        return "AddFiguresPage";
+    }
+
+    @RequestMapping("/dragFigure")
+    public String getDragFigurePage(Model model) {
+        return "DragFigurePage";
+    }
+
+    @RequestMapping("/removeFigure")
+    public String getRemoveFigurePage(Model model) {
+        return "RemoveFigurePage";
+    }
+
+    @RequestMapping("/compute")
+    public String getComputePage(Model model) {
+        return "ComputePage";
+    }
+
+    @RequestMapping("/checkIfCross")
+    public String getCheckIfCrossPage(Model model) {
+        return "CheckIfCrossPage";
     }
 }
